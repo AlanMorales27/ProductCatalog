62fdc66ceeb4caef28861cb5b7991c15632228a1
AlanMorales27 <alanmoralesg47@gmail.com>
2026-05-27 23:44:18 -0500

    feat: Add Product model and enum for product categories

---
+++ Models/Product.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+
+public enum ProductCategory
+{
+    Electronics,
+    Clothing,
+    HomeGoods,
+    Books,
+    Toys
+}
+
+public class Product
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; } = "";
+
+    [Required]
+    public string SKU { get; set; } = "";
+
+    [Range(
+        0, double.MaxValue, 
+        ErrorMessage = "El precio debe ser mayor o igual a 0"
+    )]
+    public decimal Price { get; set; }
+
+    [Range(
+        0, int.MaxValue,
+        ErrorMessage = "El stock debe ser un número entero positivo"
+    )]
+    public int Stock { get; set; }
+
+    [Required]
+    public ProductCategory Category { get; set; }
+}
